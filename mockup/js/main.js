3914458d54b12d88dd48e997168382ca458bffdc
RobStepanyan <robert1stepanyan@gmail.com>
2020-06-24 16:30:44 +0400

    js-scroll-triggr, sub sass files, services section

--- mockup/js/main.js
+++ mockup/js/main.js
@@ -7,3 +7,23 @@ var navbarCollapse = function () {
 };
 navbarCollapse();
 $(window).scroll(navbarCollapse);
+
+function scrollToTarget() {
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate({
+                    scrollTop: target.offset().top - $('a.navbar-brand').outerHeight(true),
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    })
+}
+scrollToTarget()
+$(document).resize(scrollToTarget())
+$(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+});
