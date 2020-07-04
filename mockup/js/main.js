bc4f447a43653843c2ac40cba39fec79924f4dbf
RobStepanyan <robert1stepanyan@gmail.com>
2020-07-04 17:56:41 +0400

    New navbar not yet done, minors

--- mockup/js/main.js
+++ mockup/js/main.js
@@ -39,3 +39,18 @@ $(document).ready(() => {
         infinite: true,
     });
 });
+
+// SB Sidebar
+// Add active state to sidbar nav links
+var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+$("#layoutSidenav_nav .sidenav a.nav-link").each(function () {
+    if (this.href === path) {
+        $(this).addClass("active");
+    }
+});
+
+// Toggle the side navigation
+$("#sidebarToggle").on("click", function (e) {
+    e.preventDefault();
+    $("body").toggleClass("sidenav-toggled");
+});
