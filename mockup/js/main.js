2bb94cadd864ee438f3c27c5b49ad33108061198
RobStepanyan <robert1stepanyan@gmail.com>
2020-07-17 12:27:56 +0400

    Removed unused js

--- mockup/js/main.js
+++ mockup/js/main.js
@@ -1,33 +1,3 @@
-var navbarCollapse = function () {
-    if ($(".navbar").offset().top > 10) {
-        $(".navbar").addClass("navbar-shrink");
-    } else {
-        $(".navbar").removeClass("navbar-shrink");
-    }
-};
-navbarCollapse();
-$(window).scroll(navbarCollapse);
-
-function scrollToTarget() {
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate({
-                    scrollTop: target.offset().top - $('a.navbar-brand').outerHeight(true),
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    })
-}
-scrollToTarget()
-$(document).resize(scrollToTarget())
-$(".js-scroll-trigger").click(function () {
-    $(".navbar-collapse").collapse("hide");
-});
-
 $(document).ready(() => {
     $('.carousel').slick({
         cssEase: 'ease',
@@ -55,6 +25,7 @@ $("#sidebarToggle").on("click", function (e) {
     $("body").toggleClass("sidenav-toggled");
 });
 
+// Dropdown button title change on click
 $('[data-toggle-enabled="true"] .dropdown-item').click(function () {
     $(this).parent().parent().find('.active').removeClass('active');
     $(this).addClass('active');
