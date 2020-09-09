e7d514c64270fd8c7929cd60a16ff91b61a8cfd8
RobStepanyan <robert1stepanyan@gmail.com>
2020-09-09 21:40:08 +0400

    Added django-cors-headers to secure api requests

--- setup2/setup2/settings.py
+++ setup2/setup2/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'frontend',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -162,3 +164,8 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "https://silkroadruginc.com"
+]
