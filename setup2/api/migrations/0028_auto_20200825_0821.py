0f6bc7e3a634de42dd7973d33288f9bb7c944426
RobStepanyan <robert1stepanyan@gmail.com>
2020-09-29 20:20:23 +0400

    Migration error fixes

--- setup2/api/migrations/0028_auto_20200825_0821.py
+++ setup2/api/migrations/0028_auto_20200825_0821.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0027_auto_20200825_0820'),
+        ('api', '0025_auto_20200824_1819'),
     ]
 
     operations = [
